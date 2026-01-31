611ecb6f64f04cef10444dc2a8f58465cae511a8
盛浩轩 <3526975855@qq.com>
2026-01-31 16:21:17 +0800

    项目初始化

---
+++ src/test/java/com/pixelhub/PixelHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
